0fa4d8a41045162f1fe528bf5ceae01774d82775
Егоров Александр <aegorov@neoflex.ru>
2023-06-04 17:44:00 +0300

    Cleaning repository

--- skud/skud.py
+++ skud/skud.py
@@ -10,9 +10,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 class Skud:
-    def __init__(self,
-                 feast_repo_path,
-                 ):
+    def __init__(self, feast_repo_path,):
         """
         Инициализация объекта проекта Skud.
 
@@ -22,12 +20,20 @@ class Skud:
         Returns:
             None
         """
-
         self.feast_repo_path = feast_repo_path
-
         self.feast_client = FeastClient(feast_repo_path=feast_repo_path)
 
-    def generate_parquet(self, images_folder, faces_parquet_path):
+    def generate_parquet(self, images_folder: str, faces_parquet_path: str):
+        """
+        Генерация parquet файла из исходных картинок в указанной папке.
+
+        Args:
+            images_folder (str): Путь до каталога с изображениями лиц (/path/to/image_folder/).
+            faces_parquet_path (str): Путь куда будет сохранен итоговый parquet файл (/path/to/file.parquet).
+
+        Returns:
+            None
+        """
         faces_data = []
         face_id = 0
 
@@ -55,13 +61,31 @@ class Skud:
         faces_df = pd.DataFrame(faces_data, columns=column_names)
         faces_df.to_parquet(faces_parquet_path)
 
-    def create_feast_dataset(self, feature_view_name, faces_parquet_path, faces_dataset_name):
+    def create_feast_dataset(self, feature_view_name: str, faces_parquet_path: str, faces_dataset_name: str):
+        """
+        Генерация Dataset в Feast из FeatureView и исходного parquet файла.
+
+        Args:
+            feature_view_name (str): Название существующего FeatureView в Feast.
+            faces_parquet_path (str): Путь до исходного parquet файла (/path/to/file.parquet).
+            faces_dataset_name (str): Название будущего Dataset в Feast
+        Returns:
+            None
+        """
         self.feast_client.create_dataset(feature_view_name=feature_view_name,
                                          faces_parquet_path=faces_parquet_path,
                                          faces_dataset_name=faces_dataset_name,
                                          )
 
     def get_saved_dataset_as_df(self, faces_dataset_name: str) -> pd.DataFrame:
+        """
+        Получение существующего Dataset в Feast по имени.
+
+        Args:
+            faces_dataset_name (str): Название существующего Dataset в Feast
+        Returns:
+            pd.DataFrame
+        """
         return self.feast_client.get_dataset(faces_dataset_name)
 
 
