71d6ffdc04d6dbbb204359464741d33faab08d89
TeamCity <teamcity@neoflex.ru>
2023-07-22 13:37:21 +0300

    Update arguments of script

--- src/compare_face_with_parquet.py
+++ src/compare_face_with_parquet.py
@@ -1,37 +1,64 @@
 import face_recognition
 import pandas as pd
 import numpy as np
+import argparse
 
-INPUT_PARQUET_PATH = "../datasets/group_encoding.parquet"
-INPUT_IMAGE_PATH = "../images/biden.jpg"
+INPUT_PARQUET_PATH = str
+INPUT_IMAGE_PATH = str
 
 
 def get_names_of_matching_faces(indexes, compared_output):
+    """Function return names of matching faces"""
     return [name_of_human for name_of_human, state_value in zip(indexes, compared_output) if state_value]
 
 
-df = pd.read_parquet(INPUT_PARQUET_PATH)
-print(df.head())
+def main():
+    """Main function"""
+    print(f"Input parquet path: {INPUT_PARQUET_PATH}")
+    print(f"Input image path: {INPUT_IMAGE_PATH}")
 
-biden_image = face_recognition.load_image_file(INPUT_IMAGE_PATH)
-biden_encoding = face_recognition.face_encodings(biden_image)
-if len(biden_encoding) != 0:
-    result = face_recognition.compare_faces(known_face_encodings=df,
-                                            face_encoding_to_check=biden_encoding[0]
-                                            )
-    face_distances = face_recognition.face_distance(face_encodings=df,
-                                                    face_to_compare=biden_encoding[0]
-                                                    )
-    best_match_index = np.argmin(face_distances)
+    df = pd.read_parquet(INPUT_PARQUET_PATH)
+    print(df.head())
 
-    name = "Unknown"
-    if result[best_match_index]:
-        name = df.index.values[best_match_index]
+    image = face_recognition.load_image_file(INPUT_IMAGE_PATH)
+    encoding = face_recognition.face_encodings(image)
 
-    name = df.index.values[best_match_index] if result[best_match_index] else "Unknown"
+    print(f"Len of encoding: {len(encoding)}")
+    if len(encoding) != 0:
+        result = face_recognition.compare_faces(known_face_encodings=df,
+                                                face_encoding_to_check=encoding[0]
+                                                )
+        face_distances = face_recognition.face_distance(face_encodings=df,
+                                                        face_to_compare=encoding[0]
+                                                        )
+        best_match_index = np.argmin(face_distances)
 
-    names = get_names_of_matching_faces(df.index.values, result)
-    print(f"Result: {result}")
-    print(f"Face distances: {face_distances}")
-    print(f"Names: {names}")
-    print(f"Name: {name}")
+        name = "Unknown"
+        if result[best_match_index]:
+            name = df.index.values[best_match_index]
+
+        name = df.index.values[best_match_index] if result[best_match_index] else "Unknown"
+
+        names = get_names_of_matching_faces(df.index.values, result)
+        print(f"Result: {result}")
+        print(f"Face distances: {face_distances}")
+        print(f"Names: {names}")
+        print(f"Name: {name}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get names of matching faces')
+
+    parser.add_argument('--input_parquet_path',
+                        default='../datasets/group_encoding.parquet',
+                        help='Path to parquet file of face encodings')
+    parser.add_argument('--input_image_path',
+                        default='../images/test_group/forest.jpg',
+                        help='Path to image to compare')
+
+    args = parser.parse_args()
+
+    INPUT_PARQUET_PATH = args.input_parquet_path
+    INPUT_IMAGE_PATH = args.input_image_path
+
+    main()
